f1e369b7cdaa43ebe3b85e09ec43827154e9c4ec
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-12-02 16:33:16 +0100

    move util func

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::f32::consts::PI;
+mod mesh_utils;
 
 use bevy::{
-    prelude::*, render::{
-        mesh::PrimitiveTopology, render_asset::RenderAssetUsages
-    },
+    prelude::*,
     window::PrimaryWindow,
 };
 
@@ -36,30 +34,12 @@ pub struct Player {
     rotation_speed: f32,
 }
 
-pub fn star_mesh (points: u16, radius: f32, inner_radius: f32) -> Mesh {
-    let mut positions = Vec::new();
-    let mut indices = Vec::new();
-    positions.push(Vec3::splat(0.));
-    for i in 0..(points * 2) {
-        let angle = i as f32 / points as f32 * PI;
-        let r = if i % 2 == 0 { radius } else { inner_radius };
-        positions.push(Vec3::new(r * angle.cos(), r * angle.sin(), 0.));
-        indices.push(0);
-        indices.push(i + 1);
-        indices.push((i + 1) % (2 * points) + 1);
-    }
-
-    Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default())
-        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
-        .with_inserted_indices(bevy::render::mesh::Indices::U16(indices))
-}
-
 pub fn spawn_player(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    let mesh = star_mesh (7, PLAYER_SIZE / 2., PLAYER_SIZE / 3.);
+    let mesh = mesh_utils::star_mesh (7, PLAYER_SIZE / 2., PLAYER_SIZE / 3.);
 
     commands.spawn((
         Mesh2d(meshes.add(mesh).into()),
---
+++ src/mesh_utils.rs
@@ -0,0 +1,24 @@
+use std::f32::consts::PI;
+
+use bevy::{
+    prelude::*, 
+    render::{mesh, render_asset},
+};
+
+pub fn star_mesh (points: u16, radius: f32, inner_radius: f32) -> mesh::Mesh {
+    let mut positions = Vec::new();
+    let mut indices = Vec::new();
+    positions.push(Vec3::splat(0.));
+    for i in 0..(points * 2) {
+        let angle = i as f32 / points as f32 * PI;
+        let r = if i % 2 == 0 { radius } else { inner_radius };
+        positions.push(Vec3::new(r * angle.cos(), r * angle.sin(), 0.));
+        indices.push(0);
+        indices.push(i + 1);
+        indices.push((i + 1) % (2 * points) + 1);
+    }
+
+    mesh::Mesh::new(mesh::PrimitiveTopology::TriangleList, render_asset::RenderAssetUsages::default())
+        .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, positions)
+        .with_inserted_indices(bevy::render::mesh::Indices::U16(indices))
+}
