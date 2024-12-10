8e541a649b0569314933272a7586b0a5d83dcc79
robin.hruska@teskalabs.com <byewokko@seznam.cz>
2024-12-10 23:55:33 +0100

    exit app when esc is pressed

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,12 @@ fn main() {
         .add_plugins(camera::CameraPlugin)
         .add_plugins(player::PlayerPlugin)
         .add_plugins(ui::UIPlugin)
+        .add_systems(Update, exit_app_on_esc)
         .run();
+}
+
+fn exit_app_on_esc(keys: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
+    if keys.just_pressed(KeyCode::Escape) {
+        exit.send(AppExit::Success);
+    }
 }
